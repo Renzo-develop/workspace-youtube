fc3cd64f9693214510865ad0984cbfaadb91b2cb
Amigo-Programador <Github2939>
2025-04-10 10:02:49 -0500

    levantando el proyecto usando docker-compose y variables de entorno

--- ms-client/src/main/java/com/amigo/programador/msclient/MsClientApplication.java
+++ ms-client/src/main/java/com/amigo/programador/msclient/MsClientApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MsClientApplication {
 
 	public static void main(String[] args) {
+		System.out.println(">>>>> Estamos iniciando ms-client 1 <<<<<<<<<<");
 		SpringApplication.run(MsClientApplication.class, args);
 	}
 
