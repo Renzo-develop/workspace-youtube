3d1fce8902700d2d8b94be6ea42c2ff33d9684ed
Amigo Programador <amigo.programador@outlook.com>
2025-06-11 21:49:40 -0500

    add reflect method flow on diagram draw.io

--- library/src/main/java/com/amigo/programador/library/util/DataValidaton.java
+++ library/src/main/java/com/amigo/programador/library/util/DataValidaton.java
@@ -3,19 +3,28 @@ package com.amigo.programador.library.util;
 import lombok.extern.slf4j.Slf4j;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Objects;
+
 import reactor.core.publisher.Mono;
 
 @Slf4j
 public class DataValidaton {
 
-  public static Mono<Object> isUniqueValue(String parameter, String methodName, Object repository) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+  public static <T> Mono<T> isUniqueValue(String parameter, String methodName, Object repository, Class<T> type) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 
     Method method = repository.getClass().getMethod(methodName, String.class);
-    Mono<Object> result = (Mono<Object>) method.invoke(repository, parameter);
+    Mono<T> result = (Mono<T>) method.invoke(repository, parameter);
 
     return result
-      .flatMap(r -> Mono.error(new IllegalArgumentException("Duplicate value: " + parameter)))
+      .map(r -> validateDuplicate(r))
       .switchIfEmpty(Mono.empty());
+  }
+
+  private static <T> T validateDuplicate(T t) {
+    if(Objects.nonNull(t)) {
+      throw new IllegalArgumentException("Duplicate value: " + t);
+    }
+    return null;
   }
 
 }
