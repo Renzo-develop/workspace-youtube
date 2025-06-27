0d9ec1852702cf9f01a9ca2c490a62ca9e833c24
Amigo Programador <amigo.programador@outlook.com>
2025-06-26 21:52:09 -0500

    Add gateway api server

---
+++ api-gateway/src/main/java/config/GatewayConfiguration.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+
+
+}
---
+++ api-gateway/src/main/java/filters/CustomFilter.java
@@ -0,0 +1,23 @@
+package filters;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+//@Component
+//public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+//
+//  @Override
+//  public GatewayFilter apply(Config config) {
+//    return (exchange, chain) -> {
+//      if(!exchange.getRequest().getHeaders().)
+//      return null;
+//    };
+//  }
+//
+//
+//  public static class Config {
+//    // Put the configuration properties for your filter here
+//  }
+//
+//}
